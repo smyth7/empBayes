08cf65e87756998cfc2ec176c5e145a02156b0d5
smyth <smyth7+bitbucket@gmail.com>
2017-07-11 11:19:20 +0200

    finished 3

--- 3_empBayes_estimation.R
+++ 3_empBayes_estimation.R
@@ -4,6 +4,8 @@ library(dplyr)
 library(tidyr) 
 library(Lahman)
 library(ggplot2)
+library(stats4)
+library(knitr)
 
 # Filter out pitchers
 career <- Batting %>%
@@ -27,9 +29,28 @@ career %>%
   ggplot(aes(average)) +
   geom_histogram(binwidth = .005)
 
-# estimate prior
+# estimate prior - blog
 
-library(stats4)
+# just like the graph, we have to filter for the players we actually
+# have a decent estimate of
+career_filtered <- career %>%
+  filter(AB >= 500)
+
+m <- MASS::fitdistr(career_filtered$average, dbeta,
+                    start = list(shape1 = 1, shape2 = 10))
+
+alpha0 <- m$estimate[1]
+beta0 <- m$estimate[2]
+
+career_filtered %>%
+  filter(AB > 500) %>%
+  ggplot() +
+  geom_histogram(aes(average, y = ..density..), binwidth = .005) +
+  stat_function(fun = function(x) dbeta(x, alpha0, beta0), color = "red",
+                size = 1) +
+  xlab("Batting average")
+
+# estimate prior - book
 
 career_filtered <- career %>%
   filter(AB > 500)
@@ -49,3 +70,42 @@ ab <- coef(m)
 alpha0 <- ab[1]
 beta0 <- ab[2]
 
+profile(m)
+
+career_filtered %>%
+  filter(AB > 500) %>%
+  ggplot() +
+  geom_histogram(aes(average, y = ..density..), binwidth = .005) +
+  stat_function(fun = function(x) dbeta(x, alpha0, beta0), color = "red",
+                size = 1) +
+  xlab("Batting average")
+
+# Update - conjugate prior
+
+career_eb <- career %>%
+  mutate(eb_estimate = (H + alpha0) / (AB + alpha0 + beta0))
+
+options(digits = 3)
+career_eb %>%
+  arrange(desc(eb_estimate)) %>%
+  head(5) %>%
+  kable(booktabs = TRUE)
+options(digits = 1)
+
+# note it's not necessary to filter out the low at-bats - emp Bayes does it.
+
+
+# Plot shows shrinkage to average. 
+ggplot(career_eb, aes(average, eb_estimate, color = AB)) +
+  geom_hline(yintercept = alpha0 / (alpha0 + beta0), color = "red", lty = 2) +
+  geom_point() +
+  geom_abline(color = "red") +
+  scale_colour_gradient(trans = "log", breaks = 10 ^ (1:5)) +
+  xlab("Batting average") +
+  ylab("Empirical Bayes batting average")
+# 
+# The horizontal dashed red line marks $y=\frac{\alpha_0}{\alpha_0 + \beta_0}=r sprintf("%.3f", alpha0 / (alpha0 + beta0))$. That's what we would guess someone's batting average was if we had no evidence at all (if both $H$ and $AB$ were 0). Notice that points above that line tend to move down towards it, while points below it move up.
+# 
+# The diagonal red line marks $x=y$. Points that lie close to it are the ones that didn't get shrunk at all by empirical Bayes. Notice that they're the ones with the highest number of at-bats (the brightest blue): they have enough evidence that we're willing to believe the naive batting average estimate.
+# 
+# This process is often (including in the rest of this book) called shrinkage: the process of moving all our estimates towards the average. How much it moves these estimates depends on how much evidence we have: if we have very little evidence (4 hits out of 10) we move it a lot, if we have a lot of evidence (300 hits out of 1000) we move it only a little. That's shrinkage in a nutshell: Extraordinary outliers require extraordinary evidence.
